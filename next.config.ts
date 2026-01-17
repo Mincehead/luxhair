6a7ae07f94fe278fff3a442b4fcfddf058e6e425
Tim D <mincehead@gmail.com>
2026-01-17 18:50:23 +1100

    Allow Supabase image domain in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'mdapsjwogvyhkfeocstc.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
